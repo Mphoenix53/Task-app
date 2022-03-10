1ca09cc45f94725cf5940bf8f50e760e53a97f5c
Mphoenix53 <michaelphoenix53mailbox@gmail.com>
2022-03-10 15:34:22 -0700

    rails set up server runing task db seeded tasks model done routes crud actions set up react app instealled

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  
+  namespace :api do
+    get    'task',         to:'task#index'
+    get    'task/:id',     to:'task#show'
+    post   'task',         to:'task#create'
+    put    'task/:id',     to:'task#update'
+    delete 'task/:id',     to:'task#destroy'
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Task.destroy_all
+
+Task.create(name:'Get up',         date: '2022-03-12 06:00:00' , points:50)
+Task.create(name:'Breakfast',      date: '2022-03-12 06:30:00' , points:50)
+Task.create(name:'Work out',       date: '2022-03-21 07:00:00' , points:50)
+Task.create(name:'Get dressed',    date: '2022-03-21 07:30:00' , points:50)
