0a5c86646a7d7165bc4048c15bf258ba718a6787
Mphoenix53 <michaelphoenix53mailbox@gmail.com>
2022-03-11 15:02:05 -0700

    objectives routes fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,18 @@
 Rails.application.routes.draw do
   
   namespace :api do
-    
-    get    'task',         to:'task#index'
-    get    'task/:id',     to:'task#show'
-    post   'task',         to:'task#create'
-    put    'task/:id',     to:'task#update'
-    delete 'task/:id',     to:'task#destroy'
 
-    get    'objectives',   to: 'objectives#index'
-    get    'objectives',   to: 'objectives#show'
-    post   'objectives',   to: 'objectives#create'
-    put    'objectived',   to: 'objectives#drstroy'
+    get    'task',               to: 'task#index'
+    get    'task/:id',           to: 'task#show'
+    post   'task',               to: 'task#create'
+    put    'task/:id',           to: 'task#update'
+    delete 'task/:id',           to: 'task#destroy'
+
+    get     'objectives',        to: 'objectives#index'
+    get     'objectives/:id',    to: 'objectives#show'
+    post    'objectives',        to: 'objectives#create'
+    put     'objectives/:id',    to: 'objectives#update'
+    delete  'objectived/:id',    to: 'objectives#drstroy'
 
   end
 end
