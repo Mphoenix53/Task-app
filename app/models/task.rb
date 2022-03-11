4c49aa03134bf12409ddea0a3a7046e4e9ebb24f
Mphoenix53 <michaelphoenix53mailbox@gmail.com>
2022-03-11 14:55:15 -0700

    objectives in seed file setup and are able to be created objectives routes file updated with objectives crud

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,3 @@
 class Task < ApplicationRecord
-  has_many :Objectives, dependent: :destroy
+  has_many :objectives, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
   
   namespace :api do
+    
     get    'task',         to:'task#index'
     get    'task/:id',     to:'task#show'
     post   'task',         to:'task#create'
     put    'task/:id',     to:'task#update'
     delete 'task/:id',     to:'task#destroy'
+
+    get    'objectives',   to: 'objectives#index'
+    get    'objectives',   to: 'objectives#show'
+    post   'objectives',   to: 'objectives#create'
+    put    'objectived',   to: 'objectives#drstroy'
+
   end
 end
