34bd1989bac94da8cc4f2a38d3da8562b3fa582f
Mphoenix53 <michaelphoenix53mailbox@gmail.com>
2022-03-12 08:42:01 -0700

    core of app setup pages controllers routesstubed out and rails react runing with out problems

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -4,6 +4,15 @@ const About = ()=> {
   return (
     <div>
       <h1>About</h1>
+      <p>Hello, my names Michael Phoenix, I’ve been in the trucking industry
+         for the last fifteen years as a driver. Before trucking driving 
+         I served in the Marine Core where I gained the skills to become one 
+         of the best mop pushing, bathroom scrubbing, cleaning ninjas 
+         Uncle Sam aver turned out. During my elite janitorial career 
+         I also managed to find time to squeeze in being a 
+         mechanic fixing tucks. Before serving, I grew up in a small town 
+         in the foothills of California. Whair I and enjoy mountain biking, 
+         motorcycle racing and hiking through the back country.</p>
     </div>
   )
 }
